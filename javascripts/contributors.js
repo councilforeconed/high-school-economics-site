5db7fe2cf2893dfc3e37c111aca9b9424338b50b
Steve Kinney <hello@stevekinney.net>
2014-05-30 10:58:37 -0400

    Remove browserify dependency; update copy

--- javascripts/contributors.js
+++
@@ -1,7 +0,0 @@
-var $ = require('jquery');
-
-$('.contributor-name a').on('click', function (e) {
-  e.preventDefault();
-  $(this).parents('.contributor').find('p.biography').slideToggle();
-  return this;
-});
