8aeadd3008d4c6b1d4e58ec9812bda27f106c890
Steve Kinney <hello@stevekinney.net>
2014-06-06 16:59:49 -0400

    Fix contributor list; break about me section

--- static/main.js
+++ static/main.js
@@ -1,6 +1,13 @@
+/* globals $ */
+
 $('.contributor-name a').on('click', function (e) {
   e.preventDefault();
-  $(this).parents('.contributor').find('p.biography').slideToggle();
-  $(this).parents('.contributor').find('img').slideToggle();
+  var $this = $(this);
+  $this.parents('.contributor')
+    .find('p.biography')
+    .toggle();
+  $this.parents('.contributor')
+    .find('img')
+    .toggle();
   return this;
 });
